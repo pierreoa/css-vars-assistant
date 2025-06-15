acabdcac81156a0821f686759b4ad24a472bdcea
git <stian.larsen@sparebank1.no>
2025-06-15 07:46:44 +0200

    Updates

--- src/main/kotlin/cssvarsassistant/index/indexVersion.kt
+++ src/main/kotlin/cssvarsassistant/index/indexVersion.kt
@@ -1,3 +1,3 @@
 package cssvarsassistant.index
 
-const val INDEX_VERSION = 639
+const val INDEX_VERSION = 641
--- src/main/kotlin/cssvarsassistant/settings/CssVarsAssistantSettings.kt
+++ src/main/kotlin/cssvarsassistant/settings/CssVarsAssistantSettings.kt
@@ -25,10 +25,6 @@ class CssVarsAssistantSettings : PersistentStateComponent<CssVarsAssistantSettin
     data class State(
         var showContextValues: Boolean = true,
         var allowIdeCompletions: Boolean = true,
-        var showUsageBlock: Boolean = true,
-        var showDependenciesBlock: Boolean = true,
-        var showRelatedBlock: Boolean = true,
-        var showFilesBlock: Boolean = true,
         var indexingScope: IndexingScope = IndexingScope.PROJECT_WITH_IMPORTS,
         var maxImportDepth: Int = 20,
         var sortingOrder: SortingOrder = SortingOrder.ASC
@@ -54,29 +50,6 @@ class CssVarsAssistantSettings : PersistentStateComponent<CssVarsAssistantSettin
             state.allowIdeCompletions = value
         }
 
-    var showUsageBlock: Boolean
-        get() = state.showUsageBlock
-        set(value) {
-            state.showUsageBlock = value
-        }
-
-    var showDependenciesBlock: Boolean
-        get() = state.showDependenciesBlock
-        set(value) {
-            state.showDependenciesBlock = value
-        }
-
-    var showRelatedBlock: Boolean
-        get() = state.showRelatedBlock
-        set(value) {
-            state.showRelatedBlock = value
-        }
-
-    var showFilesBlock: Boolean
-        get() = state.showFilesBlock
-        set(value) {
-            state.showFilesBlock = value
-        }
 
     var indexingScope: IndexingScope
         get() = state.indexingScope
